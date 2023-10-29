7e462638cbfef3112e4ffbd38cefe77ee1c3f4c8
mpgxc <mpgx5.c@gmail.com>
2023-10-29 03:33:20 -0300

    feat(commons): adiciona helpers de funções e tipos

---
+++ src/common/helpers.ts
@@ -0,0 +1,29 @@
+import { UUID, randomUUID } from 'node:crypto';
+
+/**
+ * Entity is a type of object that is not defined by its attributes, but rather by a thread of continuity and its identity.
+ */
+export type Entity<T = unknown> = {
+  id: UUID;
+  createdAt: Date;
+  updatedAt: Date;
+} & T;
+
+/**
+ * Aggregate is a type of entity that is the root of an aggregate.
+ */
+export type Aggregate<T> = Entity<T>;
+
+/**
+ * EntityId is a type of unique identifier for an entity.
+ */
+export type EntityId = UUID;
+
+export const entityFactory = <T>(
+  props: Omit<T, 'id' | 'updatedAt' | 'createdAt'>,
+): Entity<T> => ({
+  id: randomUUID(),
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  ...(props as T),
+});
